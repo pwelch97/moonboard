1714b3395e22fd5bafa8d0bf1eb995f2edb5552c
8cH9azbsFifZ <g@ziegenhain.com>
2021-11-29 20:55:17 +0100

    cleanup installation procedure

---
+++ install/10-prepare-raspi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Enable SPI"
+sudo sed -i 's/\#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
+
+echo "Disable Audio"
+sudo sed -i 's/\dtparam=audio=on/#dtparam=audio=on/g' /boot/config.txt
+echo blacklist snd_bcm2835 | sudo tee /etc/modprobe.d/raspi-blacklist.conf 
+
+
+# Install dependencies
+echo "Install dependencies"
+sudo apt-get update
+sudo apt-get upgrade
+
+echo "Install + build led drivers"
+sudo apt-get -y install git vim python3-pip python3-rpi.gpio gcc make build-essential
+sudo apt-get -y install libatlas-base-dev 
+sudo apt-get -y install python-dev swig scons # for building WS2811 drivers
