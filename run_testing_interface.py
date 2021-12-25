f592a4f4d47d0a9d26f5327bec7e4a6b118eab54
8cH9azbsFifZ <g@ziegenhain.com>
2021-12-25 21:25:43 +0100

    fix

--- run_testing_interface.py
+++ run_testing_interface.py
@@ -51,5 +51,11 @@ if __name__ == "__main__":
     led_layout = LED_LAYOUT.get(args.led_layout) if args.led_layout is not None else None
     MOONBOARD = MoonBoard(args.driver_type, led_layout)
 
-    holds = ["K7"]
+    #holds = ["K7"]
+    holds = {} 
+    #['START']
+    holds ["START"] = "K1"
+    holds ["TOP"] = "K7"
+    #for k in ['START', 'MOVES', 'TOP']:
+    #    for hold in holds[k]:
     MOONBOARD.show_problem(holds)
