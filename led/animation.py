3bcad62dc81b0c07880312294064826b85b85b86
8cH9azbsFifZ <g@ziegenhain.com>
2021-12-29 13:24:53 +0100

    color test

--- led/animation.py
+++ led/animation.py
@@ -189,7 +189,7 @@ class MoonBoard:
                 #leds[int(sparkPos[i])] = HeatColor(sparkCol[i])
                 #leds[int(sparkPos[i])] %= 50 # reduce brightness to 50/255
                 tmp_row = clamp(int (sparkPos[i]), 1, 18)
-                c = (100,100,0)
+                c = (sparkCol[i],0,0)
                 tmp_led = my_col + str (tmp_row)
                 self.layout.set(self.MAPPING[tmp_led], c)
 
