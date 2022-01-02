6f068daffcf209509b800188766e50d7e072dc03
8cH9azbsFifZ <g@ziegenhain.com>
2022-01-02 21:42:55 +0100

    cleanup

--- install/install.sh
+++ install/install.sh
@@ -5,20 +5,12 @@
 #./10-prepare-raspi.sh # FIXME
 #./20-prepare-python.sh # FIXME
 
-# Install dependencies
-echo "Install dependencies"
-sudo apt-get update
-sudo apt-get upgrade
 
-echo "Install + build led drivers"
-sudo apt-get -y install git vim python3-pip python3-rpi.gpio gcc make build-essential
-sudo apt-get -y install libatlas-base-dev 
-sudo apt-get -y install python-dev swig scons # for building WS2811 drivers
 
-echo "Install application"
-test -d moonboard || git clone https://github.com/8cH9azbsFifZ/moonboard.git
-cd moonboard
-git pull
+#echo "Install application"
+#test -d moonboard || git clone https://github.com/8cH9azbsFifZ/moonboard.git
+#cd moonboard
+#git pull
 
 # Installing python dependencies
 echo "Installing python dependencies"
